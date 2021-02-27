b0bb8ab0abe5bdec2ff77b6aedc9200c3bdcc7bb
Unknown <wagner_gcastro@hotmail.com>
2021-02-27 17:44:08 -0300

    #03 Retornando todos os post cadastrados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12 col-md-offset-0">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    @forelse($posts as $post)
+                        <h2>{{ $post->title }}</h2>
+                        <p>{{ $post->description }}</p>
+
+                        <br/>
+                        <span>Autor: {{ $post->user->name }} </span> |  <span>Editar: {{ $post->id }}
+                    @empty
+
+                    @endforelse
                 </div>
             </div>
         </div>
